ed27fcb24ecd35dd931b0fbec2c6892b5032c402
louis baptiste <louisvis@ymail.com>
2018-08-06 23:46:17 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,16 +4,11 @@ def Chatty_student < Student
 
   def hello
     super
-    puts "Hey there! I'm so excited to learn stuff."+"I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? 
-    You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? 
+    puts "Hey there! I'm so excited to learn stuff."+"I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night?
+    You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?
     Okay well let me just tell you who died..."
   end
 
-  def raise_hand
-    super
-    3.times do 
-      puts "Pick me!".10 times
-    end
-  end
+
 
 end
