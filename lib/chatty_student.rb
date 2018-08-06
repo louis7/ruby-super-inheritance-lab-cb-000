c9bbbb1cd042bf09e30cab5133d9d0f0282dfd31
louis baptiste <louisvis@ymail.com>
2018-08-06 23:53:30 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,11 @@
 
 
 def ChattyStudent
+  def hello
+      super
+      puts "Hey there! I'm so excited to learn stuff. I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night?
+      You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?
+      Okay well let me just tell you who died..."
+    end
 
 end
