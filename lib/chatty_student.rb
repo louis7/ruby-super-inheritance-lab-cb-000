337db338bac75aa8800721d265b69a17304c4eb3
louis baptiste <louisvis@ymail.com>
2018-08-06 23:43:09 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,1 +1,13 @@
 require 'student.rb'  
+
+def Chatty_student < Student 
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+  
+end
